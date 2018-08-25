7387db7a2623d71112afd2b2e67dada3e689732e
Fi2zz <wenjingbiao@outlook.com>
2018-08-25 20:19:51 +0800

    update

--- override.config.js
+++ override.config.js
@@ -35,43 +35,51 @@ const styleLoaders = env => {
   return [cssLoader, stylusLoader];
 };
 
+function multiPageConfig(config) {
+  //delete default entry
+  config.entry.pop();
+  if (env === envs.production) {
+    //delete react-dev-utils/webpackHotDevClient.js for production
+    config.entry.pop();
+    //turn off devtool option to disable sourcemap for production
+  }
+
+  //create new entry
+  config.entry = {
+    app1: [...config.entry, "./src/index.1.js"],
+    app: [...config.entry, "./src/index.js"]
+  };
+  //change output filename,which  default is `static/js/bundle.js`,
+  // webpack.config.dev.js of `react-scripts`,
+  if (env === envs.development) {
+    config.output.filename = "js/[name].js";
+  }
+
+  //delete HtmlWebpackPlugin for we can customize our chunks name,and
+  config.plugins = config.plugins.filter((item, index) => index !== 1);
+  //create new HtmlWebpackPlugin to separate different bundle for each page
+  config.plugins.push(
+    new HtmlWebpackPlugin({
+      filename: "app1.html",
+      template: "./public/index2.html",
+      inject: true,
+      chunks: ["app1"] //multi page key action
+    }),
+    new HtmlWebpackPlugin({
+      filename: "app.html",
+      template: "./public/index.html",
+      inject: true,
+      chunks: ["app"]
+    })
+  );
+  return config;
+}
+
 module.exports = {
   webpack(config, paths, env) {
-    //delete default entry
-    config.entry.pop();
     if (env === envs.production) {
-      //delete react-dev-utils/webpackHotDevClient.js for production
-      config.entry.pop();
-      //turn off devtool option to disable sourcemap for production
       config.devtool = false;
     }
-    //create new entry
-    config.entry = {
-      app1: [...config.entry, "./src/index.1.js"],
-      app: [...config.entry, "./src/index.js"]
-    };
-    //change output filename,which  default is `static/js/bundle.js`,
-    // webpack.config.dev.js of `react-scripts`,
-    if (env === envs.development) {
-      config.output.filename = "js/[name].js";
-    }
-    //delete HtmlWebpackPlugin for we can customize our chunks name,and
-    config.plugins = config.plugins.filter((item, index) => index !== 1);
-    //create new HtmlWebpackPlugin to separate different bundle for each page
-    config.plugins.push(
-      new HtmlWebpackPlugin({
-        filename: "index2.html",
-        template: "./public/index2.html",
-        inject: true,
-        chunks: ["app1"] //multi page key action
-      }),
-      new HtmlWebpackPlugin({
-        filename: "index.html",
-        template: "./public/index.html",
-        inject: true,
-        chunks: ["app"]
-      })
-    );
 
     //delete default  loaders, which to handler styles, we can
     //add our styles loaders,like: sass-loader,stylus-loader
@@ -82,6 +90,7 @@ module.exports = {
     });
 
     return config;
+    // return { ...multiPageConfig(config), ...config };
   },
   paths(paths) {
     return paths;
