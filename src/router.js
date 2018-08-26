2ec74bb65957adf99c7d93faaec8334bcd095a30
Fi2zz <wenjingbiao@outlook.com>
2018-08-26 12:14:18 +0800

    update

--- src/router.js
+++ src/router.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { Router, Route, Switch } from "dva/router";
-import {StoryList} from "./app/StoryList";
+import { StoryList } from "./app/StoryList";
 import { Story } from "./app/Story";
 import { StoryComments } from "./app/StoryComments";
-export default function({ history }) {
+export default function AppRouter({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route exact path="/" component={StoryList} />
         <Route exact path="/:id" namespace="app" component={Story} />
-        <Route
-          exact
-          path="/:id/comments"
-          component={StoryComments}
-        />
+        <Route exact path="/:id/comments" component={StoryComments} />
       </Switch>
     </Router>
   );
